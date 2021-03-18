ddb7d4a6ee95ab8857797e80e76b3a94a16d3058
ahinson7 <ahinson7@gatech.edu>
2021-03-17 19:49:57 -0500

    added the IP

--- client_side_judge_web_app/routes/index.js
+++ client_side_judge_web_app/routes/index.js
@@ -3,12 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { local_ip: ip });
 });
 
 router.get('/judge/:id', function(req, res, next) {
-  res.render('judge', { judge_id: req.params['id'] });
-  console.log(req.params);
+  res.render('judge', { judge_id: req.params['id']});
 });
 
 module.exports = router;
