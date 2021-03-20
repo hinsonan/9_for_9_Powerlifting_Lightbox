001f6123a494cc57f5d939a8d83cde175988e27e
ahinson7 <ahinson7@gatech.edu>
2021-03-20 17:20:35 -0500

    hotfix

--- client_side_judge_web_app/routes/index.js
+++ client_side_judge_web_app/routes/index.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const { networkInterfaces } = require('os');
 
+const nets = networkInterfaces();
+const results = Object.create(null); // Or just '{}', an empty object
+
+for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+        // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+        if (net.family === 'IPv4' && !net.internal) {
+            if (!results[name]) {
+                results[name] = [];
+            }
+            results[name].push(net.address);
+        }
+    }
+}
+
+var ip = null;
+for (key in results){
+  ip = results[key]
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { local_ip: ip });
